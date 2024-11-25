3ce9d5f4b996b99808def71950d0ccb1ef92179a
vrindasharma1 <vrindasharma2006@gmail.com>
2024-11-25 14:07:14 +0530

    update

--- Submission/FarToCel.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    float fahrenheit = 98.3;  // temperature given in F
-    float cel= (fahrenheit -32) * 5/9;   //make a new variable C and convert the respective temp into celcius
-    printf("Temp in celcius is: %f",cel);   //print the value of temp in celcius.
-    
-    return 0;
-}
